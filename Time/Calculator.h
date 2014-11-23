23d3d46f02aea9dd493b017e5c7ee31199b71969
judyge <2890408644@qq.com>
2014-11-23 20:18:22 +0800

    精确测量运行时间

---
+++ Time/Calculator.h
@@ -0,0 +1,13 @@
+class Calculator
+{
+public:
+void cal();
+};
+
+void Calculator::cal()
+{
+            for(int i=0;i<100;i++) {
+            int sum=0;
+            sum+=i;
+                                   }
+          }
